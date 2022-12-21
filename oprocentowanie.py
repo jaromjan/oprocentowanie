4be1d2759860e6832bc53d37175b7ed11a638254
Janusz Jaromin <janusz.jaromin@interia.pl>
2022-12-21 21:40:18 +0100

    Stabilna wersja dla 24 miesiecy

--- oprocentowanie.py
+++ oprocentowanie.py
@@ -26,3 +26,212 @@ komunikat = szablon.format(miesiac, kredyt_round, roznica)
 print(komunikat)
 # Przypisujemy nową kwotę kredytu
 kwota = kredyt
+# Luty 1 rok
+miesiac = "Luty"
+inflacja = -0.453509101
+kredyt =(1 + (( inflacja + procent )/1200)) * kwota - rata
+kredyt_round = round((kredyt),6)
+roznica = round((kwota - kredyt),6)
+komunikat = szablon.format(miesiac, kredyt_round, roznica)
+print(komunikat)
+kwota = kredyt
+# Marzec 1 rok
+miesiac = "Marzec"
+inflacja = 2.324671717
+kredyt =(1 + (( inflacja + procent )/1200)) * kwota - rata
+kredyt_round = round((kredyt),6)
+roznica = round((kwota - kredyt),6)
+komunikat = szablon.format(miesiac, kredyt_round, roznica)
+print(komunikat)
+kwota = kredyt
+# Kwiecień 1 rok
+miesiac = "Kwiecień"
+inflacja = 1.261254407
+kredyt =(1 + (( inflacja + procent )/1200)) * kwota - rata
+kredyt_round = round((kredyt),6)
+roznica = round((kwota - kredyt),6)
+komunikat = szablon.format(miesiac, kredyt_round, roznica)
+print(komunikat)
+kwota = kredyt
+# Maj 1 rok
+miesiac = "Maj"
+inflacja = 1.782526286
+kredyt =(1 + (( inflacja + procent )/1200)) * kwota - rata
+kredyt_round = round((kredyt),6)
+roznica = round((kwota - kredyt),6)
+komunikat = szablon.format(miesiac, kredyt_round, roznica)
+print(komunikat)
+kwota = kredyt
+# Czerwiec 1 rok
+miesiac = "Czerwiec"
+inflacja = 2.329384541
+kredyt =(1 + (( inflacja + procent )/1200)) * kwota - rata
+kredyt_round = round((kredyt),6)
+roznica = round((kwota - kredyt),6)
+komunikat = szablon.format(miesiac, kredyt_round, roznica)
+print(komunikat)
+kwota = kredyt
+# Lipiec 1 rok
+miesiac = "Lipiec"
+inflacja = 1.502229842
+kredyt =(1 + (( inflacja + procent )/1200)) * kwota - rata
+kredyt_round = round((kredyt),6)
+roznica = round((kwota - kredyt),6)
+komunikat = szablon.format(miesiac, kredyt_round, roznica)
+print(komunikat)
+kwota = kredyt
+# Sierpień 1 rok
+miesiac = "Sierpień"
+inflacja = 1.782526286
+kredyt =(1 + (( inflacja + procent )/1200)) * kwota - rata
+kredyt_round = round((kredyt),6)
+roznica = round((kwota - kredyt),6)
+komunikat = szablon.format(miesiac, kredyt_round, roznica)
+print(komunikat)
+kwota = kredyt
+# Wrzesien 1 rok
+miesiac = "Wrzesien"
+inflacja = 2.328848994
+kredyt =(1 + (( inflacja + procent )/1200)) * kwota - rata
+kredyt_round = round((kredyt),6)
+roznica = round((kwota - kredyt),6)
+komunikat = szablon.format(miesiac, kredyt_round, roznica)
+print(komunikat)
+kwota = kredyt
+# Pazdziernik 1 rok
+miesiac = "Październik"
+inflacja = 0.616921348
+kredyt =(1 + (( inflacja + procent )/1200)) * kwota - rata
+kredyt_round = round((kredyt),6)
+roznica = round((kwota - kredyt),6)
+komunikat = szablon.format(miesiac, kredyt_round, roznica)
+print(komunikat)
+kwota = kredyt
+# Listopad 1 rok
+miesiac = "Listopad"
+inflacja = 2.352295886
+kredyt =(1 + (( inflacja + procent )/1200)) * kwota - rata
+kredyt_round = round((kredyt),6)
+roznica = round((kwota - kredyt),6)
+komunikat = szablon.format(miesiac, kredyt_round, roznica)
+print(komunikat)
+kwota = kredyt
+# Grudzień 1 rok
+miesiac = "Grudzień"
+inflacja = 0.337779545
+kredyt =(1 + (( inflacja + procent )/1200)) * kwota - rata
+kredyt_round = round((kredyt),6)
+roznica = round((kwota - kredyt),6)
+komunikat = szablon.format(miesiac, kredyt_round, roznica)
+print(komunikat)
+kwota = kredyt
+# Liczymy 2 rok
+print("Rok 2")
+# Styczeń 2 rok
+miesiac = "Styczeń"
+inflacja = 1.577035247
+kredyt =(1 + (( inflacja + procent )/1200)) * kwota - rata
+kredyt_round = round((kredyt),6)
+roznica = round((kwota - kredyt),6)
+komunikat = szablon.format(miesiac, kredyt_round, roznica)
+print(komunikat)
+kwota = kredyt
+# Luty 2 rok
+miesiac = "Luty"
+inflacja = -0.292781443
+kredyt =(1 + (( inflacja + procent )/1200)) * kwota - rata
+kredyt_round = round((kredyt),6)
+roznica = round((kwota - kredyt),6)
+komunikat = szablon.format(miesiac, kredyt_round, roznica)
+print(komunikat)
+kwota = kredyt
+# Marzec 2 rok
+miesiac = "Marzec"
+inflacja = 2.48619659
+kredyt =(1 + (( inflacja + procent )/1200)) * kwota - rata
+kredyt_round = round((kredyt),6)
+roznica = round((kwota - kredyt),6)
+komunikat = szablon.format(miesiac, kredyt_round, roznica)
+print(komunikat)
+kwota = kredyt
+# Kwiecień 2 rok
+miesiac = "Kwiecień"
+inflacja = 0.267110318
+kredyt =(1 + (( inflacja + procent )/1200)) * kwota - rata
+kredyt_round = round((kredyt),6)
+roznica = round((kwota - kredyt),6)
+komunikat = szablon.format(miesiac, kredyt_round, roznica)
+print(komunikat)
+kwota = kredyt
+# Maj 2 rok
+miesiac = "Maj"
+inflacja = 1.417952672
+kredyt =(1 + (( inflacja + procent )/1200)) * kwota - rata
+kredyt_round = round((kredyt),6)
+roznica = round((kwota - kredyt),6)
+komunikat = szablon.format(miesiac, kredyt_round, roznica)
+print(komunikat)
+kwota = kredyt
+# Czerwiec 2 rok
+miesiac = "Czerwiec"
+inflacja = 1.054243267
+kredyt =(1 + (( inflacja + procent )/1200)) * kwota - rata
+kredyt_round = round((kredyt),6)
+roznica = round((kwota - kredyt),6)
+komunikat = szablon.format(miesiac, kredyt_round, roznica)
+print(komunikat)
+kwota = kredyt
+# Lipiec 2 rok
+miesiac = "Lipiec"
+inflacja = 1.480520104
+kredyt =(1 + (( inflacja + procent )/1200)) * kwota - rata
+kredyt_round = round((kredyt),6)
+roznica = round((kwota - kredyt),6)
+komunikat = szablon.format(miesiac, kredyt_round, roznica)
+print(komunikat)
+kwota = kredyt
+# Sierpień 2 rok
+miesiac = "Sierpień"
+inflacja = 1.577035247
+kredyt =(1 + (( inflacja + procent )/1200)) * kwota - rata
+kredyt_round = round((kredyt),6)
+roznica = round((kwota - kredyt),6)
+komunikat = szablon.format(miesiac, kredyt_round, roznica)
+print(komunikat)
+kwota = kredyt
+# Wrzesien 2 rok
+miesiac = "Wrzesien"
+inflacja = -0.07742069
+kredyt =(1 + (( inflacja + procent )/1200)) * kwota - rata
+kredyt_round = round((kredyt),6)
+roznica = round((kwota - kredyt),6)
+komunikat = szablon.format(miesiac, kredyt_round, roznica)
+print(komunikat)
+kwota = kredyt
+# Pazdziernik 2 rok
+miesiac = "Październik"
+inflacja = 1.165733399
+kredyt =(1 + (( inflacja + procent )/1200)) * kwota - rata
+kredyt_round = round((kredyt),6)
+roznica = round((kwota - kredyt),6)
+komunikat = szablon.format(miesiac, kredyt_round, roznica)
+print(komunikat)
+kwota = kredyt
+# Listopad 2 rok
+miesiac = "Listopad"
+inflacja = -0.404186718
+kredyt =(1 + (( inflacja + procent )/1200)) * kwota - rata
+kredyt_round = round((kredyt),6)
+roznica = round((kwota - kredyt),6)
+komunikat = szablon.format(miesiac, kredyt_round, roznica)
+print(komunikat)
+kwota = kredyt
+# Grudzień 2 rok
+miesiac = "Grudzień"
+inflacja = 1.499708521
+kredyt =(1 + (( inflacja + procent )/1200)) * kwota - rata
+kredyt_round = round((kredyt),6)
+roznica = round((kwota - kredyt),6)
+komunikat = szablon.format(miesiac, kredyt_round, roznica)
+print(komunikat)
+# kończymy
